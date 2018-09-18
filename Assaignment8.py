1e99eb1ca1d7171e644dfe85abfc06ad827ec8a9
Eyþór Brynjólfsson <eythor.brynjolfsson@gmail.com>
2018-09-18 09:29:59 +0000

    assaignment8

---
+++ Assaignment8.py
@@ -0,0 +1,16 @@
+# Verkefni 8 - Tile Traveler leikur
+
+north = "n" or "N"
+south = "s" or "S"
+west = "w" or "W"
+east = "e" or "E"
+
+position = 1
+tiles = "N"
+
+while tiles <= 9 and tiles >= 1:
+    if tiles == 1:
+        print("You can travel: (N)orth")
+        if 
+    else:
+        print("Not a valid direction!")
